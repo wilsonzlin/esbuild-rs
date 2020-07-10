d9e1de8117e86e33f73a7f349e6eb80f577c9aed
Wilson Lin <code@wilsonl.in>
2020-07-10 22:46:51 +1000

    Install Go deps when building

--- build.rs
+++ build.rs
@@ -3,8 +3,15 @@ use std::env;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
+    let gopath = format!("{}/gopath", out_dir);
 
     Command::new("go")
+        .env("GOPATH", gopath.clone())
+        .arg("get")
+        .arg("./");
+
+    Command::new("go")
+        .env("GOPATH", gopath.clone())
         .arg("build")
         .arg("-buildmode=c-archive")
         .arg("-o")
