0fceddc2ac32feeabd11990b44879cd79fb94135
Wilson Lin <code@wilsonl.in>
2020-07-10 22:17:43 +1000

    Fix premature use of OUT_DIR

--- build.rs
+++ build.rs
@@ -1,15 +1,18 @@
 use std::process::Command;
+use std::env;
 
 fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+
     Command::new("go")
         .arg("build")
         .arg("-buildmode=c-archive")
         .arg("-o")
-        .arg(format!("{}/{}", env!("OUT_DIR"), "libesbuild.a"))
+        .arg(format!("{}/{}", out_dir, "libesbuild.a"))
         .arg("esbuild.go")
         .status()
         .expect("compile Go library");
 
-    println!("cargo:rustc-link-search=native={}", env!("OUT_DIR"));
+    println!("cargo:rustc-link-search=native={}", out_dir);
     println!("cargo:rustc-link-lib=static={}", "esbuild");
 }
