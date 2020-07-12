54ef3a4db43f043dbbda987924f2e5ca6c454729
Wilson Lin <code@wilsonl.in>
2020-07-12 22:00:45 +1000

    Use parallelism in benchmark

--- nodejs/bench/run.js
+++ nodejs/bench/run.js
@@ -1,9 +1,16 @@
-const benchmark = require('benchmark');
 const fs = require('fs');
 const path = require('path');
 
 const esbuild = require('esbuild');
 const esbuildNative = require('esbuild-native');
+
+const TESTS_FILTER = new Set([
+  'react.development',
+  'c3',
+  'three',
+  'plotly',
+]);
+const ITERATIONS_PER_TEST = 100;
 
 const transformOptions = {
   minify: true,
@@ -12,31 +19,45 @@ const transformOptions = {
   minifyWhitespace: true,
 };
 
-const tests = fs.readdirSync(path.join(__dirname, 'tests')).map(f => ({
-  name: f,
-  sourceBuffer: fs.readFileSync(path.join(__dirname, 'tests', f)),
-  sourceText: fs.readFileSync(path.join(__dirname, 'tests', f), 'utf8'),
-}));
+const tests = fs.readdirSync(path.join(__dirname, 'tests')).map(f => {
+  const buf = fs.readFileSync(path.join(__dirname, 'tests', f));
+  return ({
+    name: f,
+    sourceBuffer: buf,
+    sourceText: buf.toString(),
+  });
+});
 
-// First, ensure they produce identical output.
-for (const {name, sourceBuffer, sourceText} of tests) {
-  const expected = esbuild.transformSync(sourceText, transformOptions).js;
-  const got = esbuildNative.minify(sourceBuffer).toString();
-  if (expected !== got) {
-    fs.writeFileSync('expected.js', expected);
-    fs.writeFileSync('got.js', got);
-    throw new Error(`esbuild-native output does not match esbuild for test ${name} (written to {expected,got}.js)`);
+const testMinifier = async (minifierName, minifier) => {
+  const promises = [];
+  const start = Date.now();
+  for (const {name, sourceBuffer, sourceText} of tests) {
+    if (TESTS_FILTER.has(name)) {
+      for (let i = 0; i < ITERATIONS_PER_TEST; i++) {
+        promises.push(minifier(sourceBuffer, sourceText));
+      }
+    }
   }
-}
+  await Promise.all(promises);
+  const time = Date.now() - start;
+  console.log(`${minifierName} took ${time} ms`);
+};
 
-const suite = new benchmark.Suite();
-suite.add('esbuild', () => {
-  tests.forEach(({name, sourceText}) => esbuild.transformSync(sourceText, transformOptions));
-});
-suite.add('esbuild-native', () => {
-  tests.forEach(({name, sourceBuffer}) => esbuildNative.minify(sourceBuffer));
-});
-suite
-  .on('complete', () => suite.forEach(({name, hz}) => console.log(name, hz)))
-  .on('error', console.error)
-  .run();
+(async () => {
+  for (const {name, sourceBuffer, sourceText} of tests) {
+    // First, ensure they produce identical output.
+    const expected = (await esbuild.transform(sourceText, transformOptions)).js;
+    const got = (await esbuildNative.minify(sourceBuffer)).toString();
+    if (expected !== got) {
+      fs.writeFileSync('expected.js', expected);
+      fs.writeFileSync('got.js', got);
+      throw new Error(`esbuild-native output does not match esbuild for test ${name} (written to {expected,got}.js)`);
+    }
+  }
+
+  const svc = await esbuild.startService();
+  await testMinifier('esbuild', (_, text) => svc.transform(text, transformOptions));
+  svc.stop();
+
+  await testMinifier('esbuild-rs', (buf, _) => esbuildNative.minify(buf));
+})();
