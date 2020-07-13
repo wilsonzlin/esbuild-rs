6bb6635f9c627bbe9c8ac8100d8b5a2c828074e8
Wilson Lin <code@wilsonl.in>
2020-07-13 15:37:38 +1000

    Fix API

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use std::ffi::CString;
 use std::os::raw::{c_char, c_ulonglong, c_void};
 #[cfg(feature = "use-dylib")]
 use dlopen::wrapper::{Container, WrapperApi};
@@ -33,24 +32,20 @@ struct GoString {
     b: i64,
 }
 
-pub unsafe fn esbuild_unchecked<'i, 'o>(code: &'i [u8]) -> &'o [u8] {
+pub fn esbuild<'i, 'o>(code: &'i [u8]) -> &'o [u8] {
     let go_string = GoString {
         a: code as *const [u8] as *const c_char,
         b: code.len() as i64,
     };
     let mut out_len = 0;
 
-    #[cfg(not(feature = "use-dylib"))]
-    let result = MinifyJs(go_string, &mut out_len) as *mut u8;
+    unsafe {
+        #[cfg(not(feature = "use-dylib"))]
+        let result = MinifyJs(go_string, &mut out_len) as *mut u8;
 
-    #[cfg(feature = "use-dylib")]
-    let result = DYLIB_CONT.MinifyJs(go_string, &mut out_len) as *mut u8;
+        #[cfg(feature = "use-dylib")]
+        let result = DYLIB_CONT.MinifyJs(go_string, &mut out_len) as *mut u8;
 
-    core::slice::from_raw_parts(result, out_len as usize)
-}
-
-pub fn esbuild(code: &str) -> &str {
-    let c_code = CString::new(code).expect("CString::new failed");
-    let out_bytes = unsafe { esbuild_unchecked(&c_code.as_bytes_with_nul()) };
-    std::str::from_utf8(out_bytes).expect("decoding UTF-8")
+        core::slice::from_raw_parts(result, out_len as usize)
+    }
 }
