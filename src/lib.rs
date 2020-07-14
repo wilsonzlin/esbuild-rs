d91243fff195bce4d9bbc9d4fd686477446c5e0c
Wilson Lin <code@wilsonl.in>
2020-07-14 20:39:56 +1000

    Remove dylib support

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,9 @@
 use std::os::raw::{c_char, c_ulonglong, c_void};
-#[cfg(feature = "use-dylib")]
-use dlopen::wrapper::{Container, WrapperApi};
-#[cfg(feature = "use-dylib")]
-use dlopen_derive::WrapperApi;
-#[cfg(feature = "use-dylib")]
-use lazy_static::lazy_static;
 
 mod test;
 
-#[cfg(not(feature = "use-dylib"))]
 extern "C" {
     fn MinifyJs(code: GoString, out_len: *mut c_ulonglong) -> *const c_void;
-}
-
-#[cfg(feature = "use-dylib")]
-#[derive(WrapperApi)]
-struct Api {
-    MinifyJs: unsafe extern "C" fn(code: GoString, out_len: *mut c_ulonglong) -> *const c_void,
-}
-
-#[cfg(feature = "use-dylib")]
-lazy_static! {
-    static ref DYLIB_CONT: Container<Api> = unsafe {
-        Container::load("esbuild.dll")
-    }.expect("open dynamic library");
 }
 
 #[repr(C)]
@@ -38,14 +18,8 @@ pub fn esbuild<'i, 'o>(code: &'i [u8]) -> &'o [u8] {
         b: code.len() as i64,
     };
     let mut out_len = 0;
-
     unsafe {
-        #[cfg(not(feature = "use-dylib"))]
         let result = MinifyJs(go_string, &mut out_len);
-
-        #[cfg(feature = "use-dylib")]
-        let result = DYLIB_CONT.MinifyJs(go_string, &mut out_len);
-
         core::slice::from_raw_parts(result as *mut u8, out_len as usize)
     }
 }
